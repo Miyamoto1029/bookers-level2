0ec19f799f7bb55003c408daed6f3f35e97aa1ca
Miyamoto1029 <musashimiymoto@gmail.com>
2018-10-28 13:39:35 +0900

    再提出2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :username,uniqueness: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def email_required?
+  	false
+  end
+  def email_changed?
+  	false
+  end
 
   has_many :books, dependent: :destroy
 
