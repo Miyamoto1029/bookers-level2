08bb3045f424b700c0c1e91dc3b1c0237731a5bf
Miyamoto1029 <musashimiymoto@gmail.com>
2018-10-29 15:15:54 +0900

    再提出6_authenticateの追加(except使用)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-
-	before_action :authenticate_user! #configure_permitted_parameters, if: :devise_controller?から変更
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 	protected
 
