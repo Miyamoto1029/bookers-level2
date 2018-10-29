2ba74ae723d11462664ce78e6471e41a42da567d
Miyamoto1029 <musashimiymoto@gmail.com>
2018-10-29 22:55:28 +0900

    add Last

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
+	validates :caption, length: { maximum: 200 }
 
 	belongs_to :user
 
-	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  validates :username, presence: true #uniquenessから変更
-  validates :email, presence: true #ここを追加した
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :username, presence: true #uniquenessから変更
+  validates :username,    length: { minimum: 2 }
+  validates :username,    length: { maximum: 20 }
+  validates :introduction, length: { maximum: 50 }
   has_many :books, dependent: :destroy
 
   attachment :profile_image
